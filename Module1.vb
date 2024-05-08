8399d4df817ae5dcefd15ffba1a8ff4ab6d0e826
Lang Hall <lhall@heckgrammar.co.uk>
2024-05-08 09:37:24 +0100

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine(CHAR_TO_CODE("a"))
+    End Sub
+    Function CHAR_TO_CODE(a As String) As Integer 'takes a as a parameter, this will be the users input
+        Return Asc(a) 'returns the ASCII value 
+    End Function
+End Module
