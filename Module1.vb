799c541ca4bef1eb547784fe183fce30dae6e9b4
Lang Hall <lhall@heckgrammar.co.uk>
2024-05-13 11:44:13 +0100

    better

--- Module1.vb
+++ Module1.vb
@@ -4,6 +4,6 @@
         Console.WriteLine(CHAR_TO_CODE("a"))
     End Sub
     Function CHAR_TO_CODE(a As String) As Integer 'takes a as a parameter, this will be the users input
-        Return Asc(a) 'returns the ASCII value 
+        Return Asc(a) 'returns the ASCII value from function
     End Function
 End Module
